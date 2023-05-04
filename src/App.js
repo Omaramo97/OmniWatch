f6affb6cb4db434f473c304706388bfb47f6f7d3
Omar Abdullah <Malfucious97@gmail.com>
2023-05-04 12:21:13 -0500

    UseEffect grabs genre API, display webpage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Section from "./components/Section";
+import MenuBar from "./components/MenuBar";
+import Feature from "./components/Feature";
 
 function App() {
+  const [genres, setGenres] = useState(null);
+
+  useEffect(() => {
+    const getGenres = async () => {
+      const { data } = await axios.get("/.netlify/functions/getGenre");
+      setGenres(data.genres);
+    };
+    getGenres();
+  }, []);
+
+  console.log(genres);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MenuBar />
+      <Feature />
+      <Section
+        genre={{ name: "OmniWatch Trending" }}
+        functionName={"getTrending"}
+        isLarge
+      />
+      {genres
+        ? genres.map((genre) => (
+            <Section genre={genre} key={genre.id} functionName={"getMovies"} />
+          ))
+        : null}
     </div>
   );
 }
